afe323f9a96c25aef9053df4ef4b055231c92221
Salim Tambay <tambaysalimst@gmail.com>
2026-03-23 22:14:08 +1100

    Use exact original homepage template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @posts = Post.published.limit(7)
-    render_site_shell(
-      content_partial: "pages/home_posts",
-      locals: { posts: @posts },
-      slider_partial: "pages/home_slider",
-      slider_locals: { posts: @posts.first(3) },
-      template_file: "home_template.html",
-      inject_styles: false
-    )
+    render file: Rails.public_path.join("home_template.html"), layout: false
   end
 end

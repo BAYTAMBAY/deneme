582279cc851c72e468e9c7226d74cf4967b31978
Salim Tambay <tambaysalimst@gmail.com>
2026-03-24 21:08:03 +1100

    Add game page and menu link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "pages#home"
+  get "game", to: "games#show"
 
   resources :posts, only: [:show], path: "blog"
 
